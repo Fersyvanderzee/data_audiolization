15bb1ea9325c3e8eae1b29d4acbf74c8bb292cec
Fersy van der Zee <fersyvanderzee@gmail.com>
2022-06-17 20:51:58 +0200

    Changed little code.

--- create_midi_file.py
+++ create_midi_file.py
@@ -16,6 +16,6 @@ class Midifile:
             mf.addNote(self.track, 0, pitch, self.time, duration, 100)
             self.time += duration
 
-        with open('output/' + track_name + '.mid', 'wb') as output_file:
+        with open(f'output/{track_name}.mid', 'wb') as output_file:
             mf.writeFile(output_file)
         print(f'Created midi file: {track_name}.mid')
